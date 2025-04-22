726e82624bfd0abd0d07724d04ba27cd1715458b
nayamttr <nayamttr@gmail.com>
2025-04-22 13:51:56 +0200

    Update player.hpp

--- GameActors/player.hpp
+++ GameActors/player.hpp
@@ -2,12 +2,37 @@
 #define PLAYER_HPP
 #include <string>
 
+using namespace std;
+
 class Player{
+    friend class Game;
+
     std::string m_name;
-    int m_id;
-    static int nb;
+    const unsigned int m_id;
+    unsigned int m_score = 0;
+    static unsigned int nb_players;
+
+    // Constructeur
+    Player(const std::string& name);
+
+    // Destructeur
+    ~Player();
+
+    Player(const  Player&) = delete;
+    Player& operator=(const Player&) = delete;
+
 public:
-    Player(std::string name);
-};
+    // Accesseurs en lecture
+    static int getNbPlayers() { return nb_players;}
+    const std::string getName() const {return m_name;}
+    unsigned int getId() const {return m_id;}
+    unsigned int getScore() const {return m_score;}
+
+    // Accesseurs en écriture
+    void setName(const std::string& name);
+    void addScore(unsigned int points);
+
+
+    };
 
 #endif // PLAYER_HPP
