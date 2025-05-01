ade8ad54d1974362181b52651fb50d1127e531a9
Raphaël <raphael.nl.roulin@gmail.com>
2025-05-01 10:53:41 +0200

    Modified PlayerBoard

--- Gametools/Abstract/playerboard.hpp
+++ Gametools/Abstract/playerboard.hpp
@@ -7,10 +7,14 @@
 #include "tileholder.hpp"
 
 class PlayerBoard : public TileHolder {
+    unsigned short int m_q_center;
+    unsigned short int m_r_center;
 public:
     PlayerBoard();
     void show() override;
     std::string getSaveString() const override;
+    void swiftQ(short int step = 3);
+    void swiftR(short int step = 3);
 };
 
 #endif // PLAYERBOARD_HPP
